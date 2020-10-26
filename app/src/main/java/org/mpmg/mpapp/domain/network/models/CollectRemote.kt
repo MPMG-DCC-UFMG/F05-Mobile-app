96ac1c7489dcb015d1a6daf087e4737ee38d9315
Walter José <walterjgsp@gmail.com>
2020-10-26 09:38:15 -0300

    Fix comment filed on collects not being sent

--- app/src/main/java/org/mpmg/mpapp/domain/network/models/CollectRemote.kt
+++ app/src/main/java/org/mpmg/mpapp/domain/network/models/CollectRemote.kt
@@ -7,7 +7,7 @@ data class CollectRemote(
     @field:Json(name = "id") val id: String,
     @field:Json(name = "public_work_id") val idPublicWork: String,
     @field:Json(name = "user_email") val userEmail: String,
-    @field:Json(name = "comment") val comment: String? = null,
+    @field:Json(name = "comments") val comment: String? = null,
     @field:Json(name = "date") val date: Long,
     @field:Json(name = "public_work_status") val publicWorkStatus: Int
 ) {
